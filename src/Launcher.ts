b76a78b12b183d4fb4f43c956db5167db34af412
Miro Adamy <miro.adamy@pivotree.com>
2021-12-24 12:38:24 +0100

    Downloaded

---
+++ src/Launcher.ts
@@ -0,0 +1,13 @@
+import { Server } from "./Server";
+
+
+class Launcher {
+
+    private server: Server = new Server();
+
+    public launchApp(){
+        this.server.startServer()
+    }
+}
+
+new Launcher().launchApp()
---
+++ src/Server.ts
@@ -0,0 +1,17 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http'
+
+
+export class Server {
+
+    public startServer(){
+        createServer(
+            (req: IncomingMessage, res: ServerResponse)=>{
+                console.log(`Got request from ${req.headers['user-agent']} for ${req.url}`)
+                res.write('Hello from TS server!')
+                res.end()
+            }
+
+        ).listen(8080)
+        console.log('Server started')
+    }
+}
